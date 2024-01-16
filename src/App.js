a4a1193579952b45f932ec7095a9951318f7636a
gautamyash <gautamyash37@gmail.com>
2024-01-16 13:23:26 +0530

    New Expense Item

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>This is the JSX TAG</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem (){
+    return<>
+    <h1>Expense Item Component</h1>
+           <h3>Food Rs 10</h3>
+
+           <h3>Petrol Rs 100</h3>
+           
+           <h3>Movies Rs 200</h3>
+           </>
+}
+
+export default ExpenseItem;
