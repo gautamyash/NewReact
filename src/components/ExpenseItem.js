30d96e9e598fcde603c942a4c60d925981b0bbc0
gautamyash <gautamyash37@gmail.com>
2024-01-16 13:32:49 +0530

    Adding lines in Expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
-function ExpenseItem (){
-    return<>
-    <h1>Expense Item Component</h1>
-           <h3>Food Rs 10</h3>
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>Expense Item Component</h1>
+      <h3>Food Rs 10</h3>
 
-           <h3>Petrol Rs 100</h3>
-           
-           <h3>Movies Rs 200</h3>
-           </>
+      <h3>Petrol Rs 100</h3>
+
+      <h3>Movies Rs 200</h3>
+    </div>
+  );
 }
 
 export default ExpenseItem;
