f988bbf0f05564234c8cd33f606cd0c3bf5b941c
gautamyash <gautamyash37@gmail.com>
2024-01-25 11:33:33 +0530

    Amount Change Button by use state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
   const deleteButton = (event) => {
     const clickedButton = event.target;
     const expenseItemElement = clickedButton.closest(".expense-item");
@@ -12,13 +16,18 @@ const ExpenseItem = (props) => {
       expenseItemElement.remove();
     }
   };
+  const changeAmount = () => {
+    setAmount("100");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className="expense-item__price">${amount}</div>
         <button onClick={deleteButton}>Delete Expense</button>
+        <button onClick={changeAmount}>Change Amount</button>
       </div>
     </Card>
   );
